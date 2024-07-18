e3e153e8b985538df3ce9e7e0b2070926435f5c8
Azariel-Tesfaye <tesfayeazariel@gmail.com>
2024-07-18 15:13:12 +0300

    Add existing project files to Git

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,24 @@
+import { shoe8 } from "../assets/images"
+import Button from "../components/Button"
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex max-lg:flex-col justify-between items-center gap-10 w-full max-container">
+       <div className="flex  flex-col flex-1">
+          <h2 className="text-4xl font-palanquin font-bold lg:max-w-lg">We Provide You <span className="text-coral-red">Super Quality</span> Shoes</h2>
+          <p className="lg:max-w-lg mt-4 info-text"> Ensuring premiuem comfort and style, our meticulously
+            crafted footwear is designed to elevate your experience provideing you with unmatched quality innovation and a touch of elegance <br /> our dedication
+            to detail and exellence ensures your satisfaction.
+          </p>
+          <div className="mt-9">
+            <Button label="View More"/>
+          </div>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+            <img src={shoe8} alt="Shoes *"
+            width={570} height={522}
+            className="object-contain" />
+        </div>
+    </section>
   )
 }
 
